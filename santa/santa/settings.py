f15f399ee451d705dd0dc92da6d6df4f4ad0a2c4
danielkim428 <47778502+danielkim428@users.noreply.github.com>
2020-12-06 15:55:52 +0900

    set

--- santa/santa/settings.py
+++ santa/santa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k&$4txhqf6o7lg7uvye1%$6q^pe!nhwisbcsm-ffd=x8sny98c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '128.199.215.81']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
