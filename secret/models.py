ed500dbdb16dbf5efd6133a578c04bd98e3dcd76
danielkim428 <47778502+danielkim428@users.noreply.github.com>
2020-12-07 01:10:37 +0900

    Group reveal

--- secret/models.py
+++ secret/models.py
@@ -6,6 +6,7 @@ class Group(models.Model):
     name = models.CharField(max_length=20)
     code = models.CharField(max_length=5, null=True)
     start = models.BooleanField(default=False)
+    reveal = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
